1acfe2774b097b5d37248c87c0c5b750e236c07c
900chan <forjung1@gmail.com>
2025-07-17 15:47:48 +0900

    ✨ Project STEP 1.
    Project 생성, Postgres DB 연결, Github action CI 구성

---
+++ config/settings/dev.py
@@ -0,0 +1,20 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'manage',
+        'USER': 'manager',
+        'PASSWORD': 'be11',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
